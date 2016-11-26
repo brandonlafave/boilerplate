769f2eff3179da8bf97f475d0c5c1241f09234f1
Brandon LaFave <lafaveb@gmail.com>
2016-11-26 07:53:38 -0800

    Add config folder and move karma.conf.js to config folder

---
+++ config/constants.js
@@ -0,0 +1,15 @@
+var constants = {
+
+	APP_TEMPLATES: [
+		'src/app/**/*tpl.html'
+	],
+
+	APP_JS_FILES: ['src/app/**/*.js', '!src/app/**/*.spec.js'],
+
+	VENDOR_JS_FILES: [
+		'node_modules/angular/angular.js'
+	]
+
+};
+
+module.exports = constants;
